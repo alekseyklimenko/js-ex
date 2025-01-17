854055c24733d1ff77639076fc376024368b62e9
Oleksii Klymenko <alex.klimenko@gmail.com>
2025-01-17 12:47:59 +0200

    add descriptions to tasks

--- src/leetcode/medium/add-two-numbers.js
+++ src/leetcode/medium/add-two-numbers.js
@@ -1,5 +1,25 @@
 // https://leetcode.com/problems/add-two-numbers/
 
+/*
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+*/
+
 const arr2list = (arr) => {
     let first = null;
     let prev = null;
@@ -55,18 +75,41 @@ var getList = function(num) {
     return first;
 }
 
-var addTwoNumbers = function(l1, l2) {
-    var n1 = getNumber(l1);
-    var n2 = getNumber(l2);
-    var result = n1 + n2;
-    console.log('result', result);
+const addTwoNumbers = (l1, l2) => {
+    const n1 = getNumber(l1);
+    const n2 = getNumber(l2);
+    const result = n1 + n2;
     return getList(result);
 };
 
+const addTwoNumbers = (l1, l2) => {
+};
 
-// const l1 = arr2list([2, 4, 3]);
-// const l2= arr2list([5, 6, 4]);
-const l1 = arr2list([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]);
+const l1 = {
+    val: 2,
+    next: {
+        val: 4,
+        next: {
+            val: 3,
+            next: null
+        }
+    }
+};
+
+const l2 = {
+    val: 5,
+    next: {
+        val: 6,
+        next: {
+            val: 4,
+            next: null
+        }
+    }
+};
+
+const l1 = arr2list([2, 4, 3]);
 const l2= arr2list([5, 6, 4]);
+// const l1 = arr2list([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]);
+// const l2= arr2list([5, 6, 4]);
 
 console.log(addTwoNumbers(l1, l2));
